c4490a4d1a735a13f27730964d278d7c084ebea1
kuldeepsingh55210-tech <kuldeepsingh55210@gmail.com>
2026-05-02 02:14:24 +0530

    fix: admin login password select fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -106,10 +106,10 @@ const loginWithPassword = async (req, res, next) => {
             return errorResponse(res, "Email aur password dono chahiye", 400);
         }
 
-        const broker = await Broker.findOne({ email }).select('+password');
+        const broker = await Broker.findOne({ email }).select('+password +role +isActive');
         if (!broker || !broker.isActive) {
             return errorResponse(res, "Account nahi mila", 401);
-        }
+        }s
 
         if (!broker.password) {
             return errorResponse(res, "Is account mein password set nahi hai. OTP se login karo.", 400);
